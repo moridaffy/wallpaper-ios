6eed182579a25ff616ae54bfed4691da212ed783
moridaffy <m.skryabin@familyfriend.com>
2019-02-13 12:07:16 +0300

    Project creation

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'wallpaperapp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+use_frameworks!
+
+  # Pods for wallpaperapp
+pod 'RxSwift'
+pod 'SnapKit'
+end
---
+++ wallpaperapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wallpaperapp
+//
+//  Created by Максим Скрябин on 13/02/2019.
+//  Copyright © 2019 MSKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
