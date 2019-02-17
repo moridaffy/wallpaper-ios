fa8e32552be90673d4d1ac143f047259af42c8ff
Maxim Skryabin <>
2019-02-17 20:37:07 +0300

    Dynamic blur at ImageDetailedViewController

--- wallpaperapp/View Controllers/ImageDetailedView/ImageDetailedViewController.swift
+++ wallpaperapp/View Controllers/ImageDetailedView/ImageDetailedViewController.swift
@@ -11,6 +11,8 @@ import SnapKit
 class ImageDetailedViewController: UIViewController {
   
   private weak var imageView: UIImageView!
+  private weak var blurSlider: UISlider!
+  private weak var blurView: UIView!
   private weak var tapGestureRecognizer: UITapGestureRecognizer!
   
   private var model: ImageDetailedViewModel!
@@ -23,12 +25,20 @@ class ImageDetailedViewController: UIViewController {
     setupContent()
   }
   
+  override var prefersStatusBarHidden: Bool {
+    return model.elementsHidden
+  }
+  
+  override var prefersHomeIndicatorAutoHidden: Bool {
+    return model.elementsHidden
+  }
+  
   func setup(model: ImageDetailedViewModel) {
     self.model = model
   }
   
   @objc private func imageTapped() {
-    showElements()
+    hideElements()
   }
   
   @objc private func closeButtonTapped() {
@@ -38,6 +48,16 @@ class ImageDetailedViewController: UIViewController {
   @objc private func saveButtonTapped() {
     guard let image = model.image.image else { return }
     UIImageWriteToSavedPhotosAlbum(image, self, #selector(savingCompleted(image:error:contextInfo:)), nil)
+  }
+  
+  @objc private func blurSliderValueChanged(_ sender: Any) {
+    // TODO: maybe try to generate blurred UIImage every time this value changes
+    //       and store it as a variable. If user taps save button, stored image
+    //       will be saved to photos library.
+    
+    if let slider = sender as? UISlider {
+      blurView.blurView.intensity = CGFloat(slider.value)
+    }
   }
   
   @objc private func savingCompleted(image: UIImage?, error: Error?, contextInfo: UnsafeMutableRawPointer?) {
@@ -58,16 +78,37 @@ class ImageDetailedViewController: UIViewController {
     view.addSubview(imageView)
     self.imageView = imageView
     
+    let blurView = UIView()
+    _ = blurView.blurView.setup(style: .light, intensity: 0.0)
+    view.addSubview(blurView)
+    self.blurView = blurView
+    
+    let blurSlider = UISlider()
+    blurSlider.value = 0.0
+    blurSlider.addTarget(self, action: #selector(blurSliderValueChanged(_:)), for: .valueChanged)
+    view.addSubview(blurSlider)
+    self.blurSlider = blurSlider
+    
     let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
     tapGestureRecognizer.numberOfTapsRequired = 1
     tapGestureRecognizer.numberOfTouchesRequired = 1
-    self.imageView.addGestureRecognizer(tapGestureRecognizer)
+    self.blurView.addGestureRecognizer(tapGestureRecognizer)
     self.tapGestureRecognizer = tapGestureRecognizer
   }
   
   private func setupConstraints() {
     imageView.snp.makeConstraints { (make) in
       make.leading.trailing.top.bottom.equalTo(view)
+    }
+    
+    blurView.snp.makeConstraints { (make) in
+      make.leading.trailing.top.bottom.equalTo(view)
+    }
+    
+    blurSlider.snp.makeConstraints { (make) in
+      make.leading.equalTo(view).offset(16.0)
+      make.trailing.equalTo(view).offset(-16.0)
+      make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-16.0)
     }
   }
   
@@ -79,9 +120,13 @@ class ImageDetailedViewController: UIViewController {
     }
   }
   
-  private func showElements() {
-    model.elementsHidden = !model.elementsHidden
+  private func hideElements(hide: Bool? = nil) {
+    model.elementsHidden = hide ?? !model.elementsHidden
+    setNeedsStatusBarAppearanceUpdate()
     navigationController?.setNavigationBarHidden(model.elementsHidden, animated: true)
+    UIView.animate(withDuration: 0.3) {
+      self.blurSlider.alpha = self.model.elementsHidden ? 0.0 : 1.0
+    }
   }
   
 }
