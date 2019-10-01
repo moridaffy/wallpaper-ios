fbbcc43dc997533332feb69ad899e1b6fbd81cfd
Антон Лисовой <lisovoy@arcsinus.ru>
2019-10-01 21:55:55 +0300

    UIViewController+Alerts improvements

--- wallpaperapp/Extensions/UIViewController+Alerts.swift
+++ wallpaperapp/Extensions/UIViewController+Alerts.swift
@@ -16,31 +16,28 @@ extension UIViewController {
   
   func showAlert(title: String?, body: String?, button: String?, actions: [UIAlertAction]?) {
     let alert = UIAlertController(title: title, message: body, preferredStyle: .alert)
-    for action in (actions ?? []) {
-      alert.addAction(action)
-    }
+    actions?.compactMap { $0 }.forEach { alert.addAction($0) }
+
     if let button = button {
-      let lastButton = UIAlertAction(title: button, style: .cancel, handler: { _ in
-        alert.dismiss(animated: true, completion: nil)
-      })
+      let lastButton = UIAlertAction(title: button, style: .cancel)
       alert.addAction(lastButton)
     }
     
-    if let _ = alert.popoverPresentationController {
-      alert.popoverPresentationController!.sourceView = self.view
-      alert.popoverPresentationController!.sourceRect = CGRect(x: self.view.bounds.size.width / 2.0, y: self.view.bounds.size.height / 2.0, width: 1.0, height: 1.0)
-      alert.popoverPresentationController!.permittedArrowDirections = [.down]
+    if let popoverPresentationController = alert.popoverPresentationController {
+      popoverPresentationController.sourceView = view
+      popoverPresentationController.sourceRect = CGRect(x: view.bounds.size.width / 2.0, y: view.bounds.size.height / 2.0, width: 1.0, height: 1.0)
+      popoverPresentationController.permittedArrowDirections = [.down]
     }
-    
-    present(alert, animated: true, completion: nil)
+
+    present(alert, animated: true)
   }
   
   func showAlertError(error: Error?, desc: String?, critical: Bool) {
-    var body: String = desc ?? NSLocalizedString("Unknown error occured", comment: "")
+    var body = desc ?? NSLocalizedString("Unknown error occured", comment: "")
     if let error = error {
       body += "\n" + NSLocalizedString("Description", comment: "") + ": \(error.localizedDescription)"
     }
-    let button: String? = critical ? nil : "Ок"
+    let button = critical ? nil : "Ок"
     
     showAlert(title: NSLocalizedString("Error", comment: ""), body: body, button: button, actions: nil)
   }
